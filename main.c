5dbe12b3b300593cb1034a351e8623dc332119ab
otaviopavoni <otaviohabbiano123@gmail.com>
2025-11-07 21:02:40 -0300

    Better CLI Graphics, XP and Random Death Messages

--- main.c
+++ main.c
@@ -14,15 +14,20 @@ Created by Otávio Pavoni
 struct character {
     int hearts;
     int coins;
+    int xp;
 };
-
 
 void welcome() {
     printf("\nWelcome to C-RPG. This is intended to be a simple, fast and fun terminal game for RPG fans.\nHere, you'll create your own story by opening doors of a dungeon, collecting coins, buying armour and slashing monsters.\n\nWe wish you good luck!\n");
 }
 
+void separator() {
+    printf("\n============================================\n");
+}
+
 int openDoor() {
     int answer = 0;
+    separator();
     printf("\nDo you wish to open a door? Type a number: \n1 for YES /\n2 for NO /\n3 for STATS /\n4 for STORE /\n0 for QUIT /\n\n");
     scanf("%d", &answer);
     return answer;
@@ -49,44 +54,59 @@ void getCoins(int *coins) {
 
 int fightEnemy() {
     int answer = 0;
+    separator();
     printf("\nDo you wish to fight the enemy?\n1 for YES /\n2 for NO /\n0 for QUIT/\n\n");
     scanf("%d", &answer);
     return answer;
 }
 
 void loseCoins(int *coins) {
-    int random_number = rand() % 25;
-    *coins -= random_number;
-    printf("You lost %d coins as you tried to run!\n\n", random_number);
+    int random_number = rand() % 100;
+    if (*coins > random_number) {
+        *coins -= random_number;
+        separator();
+        printf("\nYou lost %d coins as you tried to run!\n", random_number);
+    } else {
+        *coins -= *coins;
+        separator();
+        printf("\nYou lost all coins as you tried to run!\n");
+    }
+    
 }
 
-void fight(int *hearts) {
+void fight(int *hearts, int *xp) {
     int random_number = rand() % 5;
     *hearts -= random_number;
+    *xp += random_number;
+
+    separator();
 
     switch (random_number) {
         case 0:
-            printf("\nMonsters roared, spells blazed—the dungeon trembled as victory screamed through dust.\n");
+            printf("\nMonsters roared, spells blazed—the dungeon trembled as victory screamed through dust.");
             break;
         case 1:
-            printf("\nBlades flashed, monsters fell—echoes of courage rang through the shadowed crypt.\n");
+            printf("\nBlades flashed, monsters fell—echoes of courage rang through the shadowed crypt.");
             break;
         case 2:
-            printf("\nDragons soared, fire rained—only steel and will kept the heroes standing tall.\n");
+            printf("\nDragons soared, fire rained—only steel and will kept the heroes standing tall.");
             break;
         case 3:
-            printf("\nZombies swarmed, armour cracked—yet one sword's light cut through the endless night.\n");
+            printf("\nZombies swarmed, armour cracked—yet one sword's light cut through the endless night.");
             break;
         case 4:
-            printf("\nSteel clashed, dragons roared—heroes rose from fire and fury, armour blazing, victory claimed.\n");
+            printf("\nSteel clashed, dragons roared—heroes rose from fire and fury, armour blazing, victory claimed.");
             break;
     }
-    printf("You lost %d hearts as you tried to fight!\n\n", random_number);
+    printf("\n\nYou lost %d hearts as you tried to fight!\n", random_number);
+    printf("You won %d XP as you tried to fight!\n", random_number);
 }
 
 void showStats(struct character p) {
+    separator();
     printf("\nHearts: %d\n", p.hearts);
     printf("Coins: %d\n", p.coins);
+    printf("XP: %d\n", p.xp);
 }
 
 int checkHearts(struct character p) {
@@ -98,10 +118,13 @@ int checkHearts(struct character p) {
 }
 
 void store(int *hearts, int *coins){
+    separator();
     printf("\nWelcome to the store! Where your dreams come true!\n");
+    separator();
     printf("\nWEAK HEARTS POTION (restores 5 hearts) - 50 coins\n");
     printf("\nHEARTS POTION (restores 10 hearts) - 150 coins\n");
     printf("\nSTRONG HEARTS POTION (restores 20 hearts) - 300 coins\n");
+    separator();
 
     int answer = 0;
 
@@ -142,6 +165,8 @@ void store(int *hearts, int *coins){
                 printf("\nYou should try getting more coins.\n");
                 break;
             }
+        case 0:
+            break;
             
         default:
             printf("\nPlease, type a valid number!\n");
@@ -159,6 +184,7 @@ int main() {
     struct character Player;
     Player.hearts = 10;
     Player.coins = 0;
+    Player.xp = 0;
 
     while (1) {
         switch (openDoor()) {
@@ -169,12 +195,34 @@ int main() {
                 getCoins(&Player.coins);
                 switch (fightEnemy()) {
                     case 1:
-                        fight(&Player.hearts);
+                        fight(&Player.hearts, &Player.xp);
                         if (checkHearts(Player)){
                             continue;
                         } else {
-                            printf("\n********** You die. Your adventure ends here! **********\n\n\n");
-                            return 0;
+                            int random_number = rand() % 5;
+                            separator();
+                            switch (random_number) {
+                                case 0:
+                                    printf("\n********** You die. Your legend ends here... **********\n");
+                                    separator();
+                                    return 0;
+                                case 1:
+                                    printf("\n********** You die. The dungeon claims another soul. **********\n");
+                                    separator();
+                                    return 0;
+                                case 2:
+                                    printf("\n********** Game Over - better luck next time! **********\n");
+                                    separator();
+                                    return 0;
+                                case 3:
+                                    printf("\n********** You die. Your courage was not enough today. **********\n");
+                                    separator();
+                                    return 0;
+                                case 4:
+                                    printf("\n********** You die. Your adventure ends here... **********\n");
+                                    separator();
+                                    return 0;
+                            }
                         }
                         break;
                     case 2:
@@ -184,8 +232,9 @@ int main() {
                         return 0;
                     default:
                         printf("Type a valid number!");
+                        break;
                 }
-                break;
+            
             case 2:
                 continue;
             case 3:
@@ -197,7 +246,7 @@ int main() {
             default:
                 printf("\nType a valid number!\n");
         }
-    }
+        }
 
     return 0;
 }
